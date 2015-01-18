b216794c29cd01402a725aee9e1e1b6f9d3c918a
hooor <freewave_3@hotmail.com>
2015-01-19 00:39:51 +0900

    test

--- app/src/main/java/info/yokkora/sudoku/EasyActivity.java
+++ app/src/main/java/info/yokkora/sudoku/EasyActivity.java
@@ -5,12 +5,52 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class EasyActivity extends ActionBarActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy);
+
+        // timeをすすめる
+
+        // 問題作成
+
+        // gridに入れる数字を選ぶ
+        setNumbers();
+
+        // idをクリックすると選んだ数字を入れる
+
+        // 回答する
+
+        // intentのまとめ
+        setButtonIntentListener();
+    }
+
+    // gridに入れる数字を選ぶ
+    private void setNumbers() {
+        final TextView num1 = (TextView) this.findViewById(R.id.num1);
+        num1.setText("hoge");
+        num1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                num1.setText("hogehoge2");
+            }
+        });
+
+        /*
+        for (int i = 1; i < 7; ++i) {
+            TextView textview = (TextView) this.findViewById(R.id.num . i);
+            ImageButton imgbtn = (ImageButton)findViewById(imageButtons[i]);
+            nums[i] = new AnswerNum(imgbtn, i);
+        }
+        */
+    }
+
+    // intentのまとめ
+    private void setButtonIntentListener() {
         Button btnToIndex = (Button) this.findViewById(R.id.to_index);
         btnToIndex.setOnClickListener(new View.OnClickListener() {
             @Override
